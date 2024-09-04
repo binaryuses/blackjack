75b329eab2d59d91dd614838c704e9d2a09916f1
binary <159871946+binaryuses@users.noreply.github.com>
2024-09-04 18:12:38 -0400

    update..

--- main.py
+++ main.py
@@ -1,56 +1,83 @@
 import random
+import math
 
+suits = ["♥", "♦️", "♣️", "♠️"]
+
+def randomSuit(name):
+       name = (name + " " + str(random.choice(suits)))
+       
 class Card():
-    def __init__(self, name):
+    def __init__(self, name, dealers):
         self.name = name;
         self.isAce = round(random.uniform(1, 52));
-        self.value = round(random.uniform(1, 10));
-    def checkForAce(self):
-        if self.isAce == 1 or self.isAce == 2:
+        self.value = round(random.uniform(1, 13));
+        self.dealer = dealers
+
+    def init(self):
+        if self.isAce == 1 or self.isAce == 2 or self.isAce == 3 or self.isAce == 4:
             while True:
               aceChoice = input("[ACE!]: 11 or 1? >> ")
               x = int(aceChoice)
               if x == 11 or x == 1:
                 print("updated to " + str(x))
                 self.value = round(x)
+                self.name = randomSuit("ACE")
                 break
+        if self.value == 11:
+            self.name = randomSuit("J")
+        elif self.value == 12:
+            self.name = randomSuit("Q")
+        elif self.value == 13:
+            self.name = randomSuit("K")
 
-card = Card("Card")
-card2 = Card("Card")
+card = Card("Card", False)
+card2 = Card("Card", False)
 
-dealercard = Card("Card")
-dealercard2 = Card("Card")
+dealercard = Card("Card", True)
+dealercard2 = Card("Card", True)
 
-card.checkForAce()
-card2.checkForAce()
+card.init()
+card2.init()
 
-dealercard.checkForAce()
-dealercard2.checkForAce()
+dealercard.init()
+dealercard2.init()
 
-print("Dealer has " + str(dealercard.value) + ", UNKNOWN")
-print("Hit or stay? " + str(card.value) + ", " +  str(card2.value) + " : " + card.value + card2.value)
+print("Dealer's Hand: " + str(dealercard.value) + "\n" + str(dealercard.name) + "\n [UNKNOWN..]")
+print("Your Hand: " + str(int(card.value) + int(card2.value)) + "\n" + str(card.name) + "\n" + str(card2.name) + "\n Hit or Stay?")
 
-user_input = input(">> ")
+def checkResult(card: Card):
+   if card.dealer == True:
+      dealerTotal = dealercard.value + dealercard2.value
+      dealerDis = math.dist(dealerTotal, 21)
 
-def checkResult():
-   pass
+      userTotal = card.value + card2.value
+      userDis = math.dist(userTotal, 21)
 
+      if userDis < dealerDis:
+        print("I think this means u win \n dealer: " + str(dealerTotal) + "\n  yours:" + str(userTotal))
+      elif userDis == dealerDis:
+        print("draw lol \n dealer: " + str(dealerTotal) + "\n  yours:" + str(userTotal))
+      elif userDis > dealerDis:
+        print("I think this means u LOST \n dealer: " + str(dealerTotal) + "\n  yours:" + str(userTotal))
+ 
 def hit():
+   card3 = Card("Card", False)
+   checkResult(card3)
    print("hit function")
 
 def stay():
+   dealercard3 = Card("Card", True)
+   checkResult(dealercard3)
    print("stay function")
 
 def ask():
     while True:
+        user_input = input("Hit/Stay >> ")
         if user_input.lower() == "hit":
-         print("Player hit")
          hit()
          break
-        elif user_input.lower == "stay":
-         print("Player pass")
+        elif user_input.lower() == "stay":
          stay()
          break
-        else: print("Hit or stay..")
-
+        else: user_input = input("Hit/Stay >> ")
 ask()
